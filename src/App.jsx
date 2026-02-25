a88687128a9a28890c01d4b1bc26d911e8b85e69
Hridhik A D <hkbrillianthunt@gmail.com>
2026-02-25 12:38:58 +0000

    Initial commit: Gypsum plaster landing page setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Hero from './components/sections/Hero'
+import Benefits from './components/sections/Benefits'
+import Footer from './components/layout/Footer'
+
+function App() {
+  return (
+    <div className="min-h-screen">
+      <Hero />
+      <Benefits />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
